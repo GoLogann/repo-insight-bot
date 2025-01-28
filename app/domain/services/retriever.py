493ce9d927c24f991b7ba256a20287e03ae21e24
Logan Cardoso <logan.cardoso@dgtallab.com.br>
2025-01-28 01:33:11 -0300

    fix: realizando ajustes e incrementos no metodo retrieve_relevant_documents após o uso do qdrantdb

--- app/domain/services/retriever.py
+++ app/domain/services/retriever.py
@@ -1,3 +1,5 @@
+from typing import List
+
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 
@@ -6,13 +8,29 @@ class DocumentRetriever:
     def __init__(self, vector_store):
         self.vector_store = vector_store
 
-    def retrieve_relevant_documents(self, query_embedding, top_k=4):
-        embeddings, documents = self.vector_store.get_all()
+    def retrieve_relevant_documents(self, repo_url: str, query_embedding: List[float], top_k: int = 4):
+        documents = self.vector_store.get_all(repo_url)
+
+        embeddings = []
+        texts = []
+
+        for doc in documents:
+            if doc.embedding:
+                embeddings.append(doc.embedding)
+                texts.append(doc.text)
+            else:
+                embeddings.append([0] * len(query_embedding))
+                texts.append(doc.text)
 
         embeddings = np.array(embeddings)
-        query_embedding = np.array(query_embedding).reshape(1, -1)
 
+        if embeddings.shape[1] == 0:
+            raise ValueError("Embeddings have no features, cannot perform cosine similarity.")
+
+        query_embedding = np.array(query_embedding).reshape(1, -1)
         similarities = cosine_similarity(query_embedding, embeddings)[0]
+
         top_indices = np.argsort(similarities)[-top_k:][::-1]
 
-        return [documents[i] for i in top_indices]
+        return [texts[i] for i in top_indices]
+
