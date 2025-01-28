054f5d7bf2f322f668230b7acf6815a9b66a86ae
Logan Cardoso <logan.cardoso@dgtallab.com.br>
2025-01-28 01:36:47 -0300

    fix: novas configurações

--- app/core/config.py
+++ app/core/config.py
@@ -1,9 +1,9 @@
-
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    OPENAI_API_KEY: str  = ""
-    CHROMA_DB_PATH: str = "./data/chroma_db"
+    MODEL_NAME_EMBEDDING: str = "all-MiniLM-L6-v2"
+    MODEL_NAME_LLM: str = "deepseek-r1:7b"
+    QDRANT_URL: str = "http://localhost:6333"
     CHUNK_SIZE: int = 350
     CHUNK_OVERLAP: int = 20
     TOP_K_DOCUMENTS: int = 4
