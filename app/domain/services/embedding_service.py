950419deb75e8f7abe89337fca083de34f104089
Logan Cardoso <logan.cardoso@dgtallab.com.br>
2025-01-28 01:35:26 -0300

    refactor: substituindo serviço de embbeding da openAI para o setence tensformers

--- app/domain/services/embedding_service.py
+++ app/domain/services/embedding_service.py
@@ -1,7 +1,8 @@
-from app.infrastructure.openai.embedding_client import OpenAIEmbeddingClient
+from app.infrastructure.sentence_transformers.embedding_client import SentenceTransformersEmbeddingClient
+
 
 class EmbeddingService:
-    def __init__(self, embedding_client: OpenAIEmbeddingClient):
+    def __init__(self, embedding_client: SentenceTransformersEmbeddingClient):
         self.embedding_client = embedding_client
 
     def generate_embeddings(self, chunks):
